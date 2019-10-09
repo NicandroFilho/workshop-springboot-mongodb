43640e8c96b25bd4b4e61e552befeaacf7be558e
NicandroFilho <nicandro.m@gmail.com>
2019-10-09 15:04:16 -0300

    Entity User e REST funcionando (Inicio das Entities Post e Comment pra esborço de Associações)

---
+++ src/main/java/com/nicandrofilho/workshopmongo/domain/Post.java
@@ -0,0 +1,93 @@
+package com.nicandrofilho.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private Instant date;
+	private String title;
+	private String body;
+	
+	private User author;
+	private Set<Comment> comments = new HashSet<>();
+	
+	public Post() {
+	}
+
+	public Post(String id, Instant date, String title, String body, User author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Set<Comment> getComments() {
+		return comments;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
