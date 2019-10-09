58312bb5c3f21be82a7db695376d3d34cbd9dfa5
NicandroFilho <nicandro.m@gmail.com>
2019-10-09 16:55:07 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/nicandrofilho/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nicandrofilho.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nicandrofilho.workshopmongo.domain.User;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
